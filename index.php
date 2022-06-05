f9b43dc441078289b5bd430da6369fa6f91bdfaf
slimeman <100768165+uenofuuma@users.noreply.github.com>
2022-06-05 11:05:57 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>slime php</title>
+  </head>
+  <body>
+    <h1>slime test</h1>
+    <?php
+    echo "<p>hello slime!</p>";
+    $slime = "slime power";
+    echo $slime;
+    ?>
+  </body>
+</html>
